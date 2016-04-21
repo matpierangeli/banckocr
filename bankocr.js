19029dbb430cf1b142a5844e1de8bd95eb522a05
Matteo Pierangeli <matteo.pierangeli@yoox.com>
2016-04-21 17:51:27 +0200

    handle dirty read and wrong checksum

--- bankocr.js
+++ bankocr.js
@@ -1,3 +1,15 @@
+var zero  = { segment : " _ | ||_|", digit : "0" };
+var one   = { segment : "     |  |", digit : "1" };
+var two   = { segment : " _  _||_ ", digit : "2" };
+var three = { segment : " _  _| _|", digit : "3" };
+var four  = { segment : "   |_|  |", digit : "4" };
+var five  = { segment : " _ |_  _|", digit : "5" };
+var six   = { segment : " _ |_ |_|", digit : "6" };
+var seven = { segment : " _   |  |", digit : "7" };
+var eight = { segment : " _ |_||_|", digit : "8" };
+var nine  = { segment : " _ |_| _|", digit : "9" };
+var digits = [zero, one, two, three, four, five, six, seven, eight, nine];
+
 var firstReduce = function(acc, e){
   var charArray = e.split("");
   return charArray.reduce(secondReduce, acc);
@@ -13,32 +25,13 @@ var secondReduce = function(acc, e, i, arr){
   return acc;
 }
 
-var zero  = { segment : " _ | ||_|", digit : 0 };
-var one   = { segment : "     |  |", digit : 1 };
-var two   = { segment : " _  _||_ ", digit : 2 };
-var three = { segment : " _  _| _|", digit : 3 };
-var four  = { segment : "   |_|  |", digit : 4 };
-var five  = { segment : " _ |_  _|", digit : 5 };
-var six   = { segment : " _ |_ |_|", digit : 6 };
-var seven = { segment : " _   |  |", digit : 7 };
-var eight = { segment : " _ |_||_|", digit : 8 };
-var nine  = { segment : " _ |_| _|", digit : 9 };
-var digits = [zero, one, two, three, four, five, six, seven, eight, nine];
-
-/*
-var papersNumbers = ["    _  _     _  _  _  _  _ ",
-                     "  | _| _||_||_ |_   ||_||_|",
-                     "  ||_  _|  | _||_|  ||_| _|"];
-papersNumbers.forEach(function(e) { console.log(e); });
-*/
-
-var translatedDigits = function(papersNumbers) {
+var convertDigits = function(papersNumbers) {
   var translated = papersNumbers.map(function(e) {
     var candidate = digits.find(function(i) { return i.segment === e; });
     return candidate != undefined ? candidate.digit : "?";
   });
 
-  var invalid = translated.some(function(e){ e != '?'});
+  var invalid = translated.some(function(e){ return e === "?"});
 
   var checksum = translated.slice().reverse().reduce(function(acc,e,i){
     return acc += parseInt(e)*(i+1);
@@ -53,31 +46,18 @@ var translatedDigits = function(papersNumbers) {
 
 module.exports = function(papersNumbers){
   var splittedPapersNumbers = papersNumbers.reduce(firstReduce, new Array());
-  /*
-  var translatedDigits = splittedPapersNumbers.map(function(e) {
-    var candidate = digits.find(function(i) { return i.segment === e; });
-    return candidate != undefined ? candidate.digit : "?";
-  });
+  var translatedDigits = convertDigits(splittedPapersNumbers);
 
-  var digitsChecksum = translatedDigits.slice().reverse().reduce(function(acc,e,i){
-    return acc += parseInt(e)*(i+1);
-  }, 0) % 11;
-  var invalid = translatedDigits.some(function(e){ e != '?'});
+  var toPrint = translatedDigits.digits.join('');
+  if(translatedDigits.invalid)
+   toPrint += " ILL";
+  else if(translatedDigits.checksum != 0)
+     toPrint += " ERR";
 
   return {
-    digits : translatedDigits.join(''),
-    invalid : invalid,
-    checksum : invalid ? undefined : digitsChecksum,
-    print : function() { return this.digits + (invalid) ? "ILL" : "" + (checksum != 0) ? "ERR" : "" } //NOT WORK
-  }
-  */
-  var tDigits = translatedDigits(splittedPapersNumbers);
-  return {
-    digits : tDigits.digits.join(''),
-    invalid : tDigits.invalid,
-    checksum : tDigits.checksum,
-    print : function() { return this.digits + (tDigits.invalid) ? "ILL" : "" + (tDigits.checksum != 0) ? "ERR" : "" } //NOT WORK
+    digits : translatedDigits.digits,
+    invalid : translatedDigits.invalid,
+    checksum : translatedDigits.checksum,
+    toString : function() { return toPrint; }
   }
 }
-
-//console.log("\n>", convertedDigits);
--- test/bankocrtests.js
+++ test/bankocrtests.js
@@ -8,14 +8,14 @@ describe('BanckOCR', function() {
                            "| || || || || || || || || |",
                            "|_||_||_||_||_||_||_||_||_|"];
 
-      assert.equal("000000000", ocr(papersNumbers).digits);
+      assert.equal("000000000", ocr(papersNumbers).toString());
     });
     it('123456789', function () {
       var papersNumbers = ["    _  _     _  _  _  _  _ ",
                            "  | _| _||_||_ |_   ||_||_|",
                            "  ||_  _|  | _||_|  ||_| _|"];
 
-      assert.equal("123456789", ocr(papersNumbers).digits);
+      assert.equal("123456789", ocr(papersNumbers).toString());
     });
   });
   describe('checksum', function () {
@@ -39,6 +39,24 @@ describe('BanckOCR', function() {
                            " _||_|  ||_| _|  | _||_   |"];
 
       assert.equal(10, ocr(papersNumbers).checksum);
+    });
+  });
+  describe('dirty segment', function () {
+    it('recognize dirty segment', function () {
+      var papersNumbers = [" _     _  _  _  _  _  _  _ ",
+                           " _||_||  |_||   _||_||_ |_ ",
+                           " _|  | _||_||_||_  _ |_| _|"];
+
+      assert.equal("34?8?2?65 ILL", ocr(papersNumbers).toString());
+    });
+  });
+  describe('wrong account number', function () {
+    it('recognize wrong checksum', function () {
+      var papersNumbers = [" _  _  _  _  _     _  _    ",
+                           "|_||_|  ||_ |_ |_| _| _|  |",
+                           " _||_|  ||_| _|  | _||_   |"];
+
+      assert.equal("987654321 ERR", ocr(papersNumbers).toString());
     });
   });
 });
