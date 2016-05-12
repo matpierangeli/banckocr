791ac909ac1ef10096c2df137c5a3fe40449c819
matpierangeli <mat.pierangeli@gmail.com>
2016-05-12 10:10:34 +0200

    introduced recursive strategy to read segments

--- test/bankocrtests.js
+++ test/bankocrtests.js
@@ -1,21 +1,22 @@
 var assert = require('chai').assert;
-var ocr = require('../lib/bankocr.js');
+var ocrDoubleReduce = require('../lib/bankocr.js').doubleReduce;
+var ocrRecursiveSlice = require('../lib/bankocr.js').recursiveSlice;
 
-describe('BanckOCR', function() {
+describe('BanckOCR - DoubleReduce', function() {
   describe('parse', function () {
     it('000000000', function () {
       var papersNumbers = [" _  _  _  _  _  _  _  _  _ ",
                            "| || || || || || || || || |",
                            "|_||_||_||_||_||_||_||_||_|"];
 
-      assert.equal("000000000", ocr(papersNumbers).toString());
+      assert.equal("000000000", ocrDoubleReduce(papersNumbers).toString());
     });
     it('123456789', function () {
       var papersNumbers = ["    _  _     _  _  _  _  _ ",
                            "  | _| _||_||_ |_   ||_||_|",
                            "  ||_  _|  | _||_|  ||_| _|"];
 
-      assert.equal("123456789", ocr(papersNumbers).toString());
+      assert.equal("123456789", ocrDoubleReduce(papersNumbers).toString());
     });
   });
   describe('checksum', function () {
@@ -24,21 +25,21 @@ describe('BanckOCR', function() {
                            "| || || || || || || || || |",
                            "|_||_||_||_||_||_||_||_||_|"];
 
-      assert.equal(0, ocr(papersNumbers).checksum);
+      assert.equal(0, ocrDoubleReduce(papersNumbers).checksum);
     });
     it('345882865 is 0', function () {
       var papersNumbers = [" _     _  _  _  _  _  _  _ ",
                            " _||_||_ |_||_| _||_||_ |_ ",
                            " _|  | _||_||_||_ |_||_| _|"];
 
-      assert.equal(0, ocr(papersNumbers).checksum);
+      assert.equal(0, ocrDoubleReduce(papersNumbers).checksum);
     });
     it('987654321 is 10', function () {
       var papersNumbers = [" _  _  _  _  _     _  _    ",
                            "|_||_|  ||_ |_ |_| _| _|  |",
                            " _||_|  ||_| _|  | _||_   |"];
 
-      assert.equal(10, ocr(papersNumbers).checksum);
+      assert.equal(10, ocrDoubleReduce(papersNumbers).checksum);
     });
   });
   describe('dirty segment', function () {
@@ -47,7 +48,7 @@ describe('BanckOCR', function() {
                            " _||_||  |_||   _||_||_ |_ ",
                            " _|  | _||_||_||_  _ |_| _|"];
 
-      assert.equal("34?8?2?65 ILL", ocr(papersNumbers).toString());
+      assert.equal("34?8?2?65 ILL", ocrDoubleReduce(papersNumbers).toString());
     });
   });
   describe('wrong account number', function () {
@@ -56,7 +57,67 @@ describe('BanckOCR', function() {
                            "|_||_|  ||_ |_ |_| _| _|  |",
                            " _||_|  ||_| _|  | _||_   |"];
 
-      assert.equal("987654321 ERR", ocr(papersNumbers).toString());
+      assert.equal("987654321 ERR", ocrDoubleReduce(papersNumbers).toString());
+    });
+  });
+});
+
+describe('BanckOCR - RecursiveSlice', function() {
+  describe('parse', function () {
+    it('000000000', function () {
+      var papersNumbers = [" _  _  _  _  _  _  _  _  _ ",
+                           "| || || || || || || || || |",
+                           "|_||_||_||_||_||_||_||_||_|"];
+
+      assert.equal("000000000", ocrRecursiveSlice(papersNumbers).toString());
+    });
+    it('123456789', function () {
+      var papersNumbers = ["    _  _     _  _  _  _  _ ",
+                           "  | _| _||_||_ |_   ||_||_|",
+                           "  ||_  _|  | _||_|  ||_| _|"];
+
+      assert.equal("123456789", ocrRecursiveSlice(papersNumbers).toString());
+    });
+  });
+  describe('checksum', function () {
+    it('000000000 is 0', function () {
+      var papersNumbers = [" _  _  _  _  _  _  _  _  _ ",
+                           "| || || || || || || || || |",
+                           "|_||_||_||_||_||_||_||_||_|"];
+
+      assert.equal(0, ocrRecursiveSlice(papersNumbers).checksum);
+    });
+    it('345882865 is 0', function () {
+      var papersNumbers = [" _     _  _  _  _  _  _  _ ",
+                           " _||_||_ |_||_| _||_||_ |_ ",
+                           " _|  | _||_||_||_ |_||_| _|"];
+
+      assert.equal(0, ocrRecursiveSlice(papersNumbers).checksum);
+    });
+    it('987654321 is 10', function () {
+      var papersNumbers = [" _  _  _  _  _     _  _    ",
+                           "|_||_|  ||_ |_ |_| _| _|  |",
+                           " _||_|  ||_| _|  | _||_   |"];
+
+      assert.equal(10, ocrRecursiveSlice(papersNumbers).checksum);
+    });
+  });
+  describe('dirty segment', function () {
+    it('recognize dirty segment', function () {
+      var papersNumbers = [" _     _  _  _  _  _  _  _ ",
+                           " _||_||  |_||   _||_||_ |_ ",
+                           " _|  | _||_||_||_  _ |_| _|"];
+
+      assert.equal("34?8?2?65 ILL", ocrRecursiveSlice(papersNumbers).toString());
+    });
+  });
+  describe('wrong account number', function () {
+    it('recognize wrong checksum', function () {
+      var papersNumbers = [" _  _  _  _  _     _  _    ",
+                           "|_||_|  ||_ |_ |_| _| _|  |",
+                           " _||_|  ||_| _|  | _||_   |"];
+
+      assert.equal("987654321 ERR", ocrRecursiveSlice(papersNumbers).toString());
     });
   });
 });
