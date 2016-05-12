f705ecf1f252b33b152df7cb52ccd40ab4f5a529
Matteo Pierangeli <matteo.pierangeli@yoox.com>
2016-05-12 11:37:45 +0200

    replaced functions by arrow functions

--- test/bankocrtests.js
+++ test/bankocrtests.js
@@ -2,16 +2,16 @@ var assert = require('chai').assert;
 var ocrDoubleReduce = require('../lib/bankocr.js').doubleReduce;
 var ocrRecursiveSlice = require('../lib/bankocr.js').recursiveSlice;
 
-describe('BanckOCR - DoubleReduce', function() {
-  describe('parse', function () {
-    it('000000000', function () {
+describe('BanckOCR - DoubleReduce', () => {
+  describe('parse', () => {
+    it('000000000', () => {
       var papersNumbers = [" _  _  _  _  _  _  _  _  _ ",
                            "| || || || || || || || || |",
                            "|_||_||_||_||_||_||_||_||_|"];
 
       assert.equal("000000000", ocrDoubleReduce(papersNumbers).toString());
     });
-    it('123456789', function () {
+    it('123456789', () => {
       var papersNumbers = ["    _  _     _  _  _  _  _ ",
                            "  | _| _||_||_ |_   ||_||_|",
                            "  ||_  _|  | _||_|  ||_| _|"];
@@ -19,22 +19,22 @@ describe('BanckOCR - DoubleReduce', function() {
       assert.equal("123456789", ocrDoubleReduce(papersNumbers).toString());
     });
   });
-  describe('checksum', function () {
-    it('000000000 is 0', function () {
+  describe('checksum', () => {
+    it('000000000 is 0', () => {
       var papersNumbers = [" _  _  _  _  _  _  _  _  _ ",
                            "| || || || || || || || || |",
                            "|_||_||_||_||_||_||_||_||_|"];
 
       assert.equal(0, ocrDoubleReduce(papersNumbers).checksum);
     });
-    it('345882865 is 0', function () {
+    it('345882865 is 0', () => {
       var papersNumbers = [" _     _  _  _  _  _  _  _ ",
                            " _||_||_ |_||_| _||_||_ |_ ",
                            " _|  | _||_||_||_ |_||_| _|"];
 
       assert.equal(0, ocrDoubleReduce(papersNumbers).checksum);
     });
-    it('987654321 is 10', function () {
+    it('987654321 is 10', () => {
       var papersNumbers = [" _  _  _  _  _     _  _    ",
                            "|_||_|  ||_ |_ |_| _| _|  |",
                            " _||_|  ||_| _|  | _||_   |"];
@@ -42,8 +42,8 @@ describe('BanckOCR - DoubleReduce', function() {
       assert.equal(10, ocrDoubleReduce(papersNumbers).checksum);
     });
   });
-  describe('dirty segment', function () {
-    it('recognize dirty segment', function () {
+  describe('dirty segment', () => {
+    it('recognize dirty segment', () => {
       var papersNumbers = [" _     _  _  _  _  _  _  _ ",
                            " _||_||  |_||   _||_||_ |_ ",
                            " _|  | _||_||_||_  _ |_| _|"];
@@ -51,8 +51,8 @@ describe('BanckOCR - DoubleReduce', function() {
       assert.equal("34?8?2?65 ILL", ocrDoubleReduce(papersNumbers).toString());
     });
   });
-  describe('wrong account number', function () {
-    it('recognize wrong checksum', function () {
+  describe('wrong account number', () => {
+    it('recognize wrong checksum', () => {
       var papersNumbers = [" _  _  _  _  _     _  _    ",
                            "|_||_|  ||_ |_ |_| _| _|  |",
                            " _||_|  ||_| _|  | _||_   |"];
@@ -62,16 +62,16 @@ describe('BanckOCR - DoubleReduce', function() {
   });
 });
 
-describe('BanckOCR - RecursiveSlice', function() {
-  describe('parse', function () {
-    it('000000000', function () {
+describe('BanckOCR - RecursiveSlice', () => {
+  describe('parse', () => {
+    it('000000000', () => {
       var papersNumbers = [" _  _  _  _  _  _  _  _  _ ",
                            "| || || || || || || || || |",
                            "|_||_||_||_||_||_||_||_||_|"];
 
       assert.equal("000000000", ocrRecursiveSlice(papersNumbers).toString());
     });
-    it('123456789', function () {
+    it('123456789', () => {
       var papersNumbers = ["    _  _     _  _  _  _  _ ",
                            "  | _| _||_||_ |_   ||_||_|",
                            "  ||_  _|  | _||_|  ||_| _|"];
@@ -79,22 +79,22 @@ describe('BanckOCR - RecursiveSlice', function() {
       assert.equal("123456789", ocrRecursiveSlice(papersNumbers).toString());
     });
   });
-  describe('checksum', function () {
-    it('000000000 is 0', function () {
+  describe('checksum', () => {
+    it('000000000 is 0', () => {
       var papersNumbers = [" _  _  _  _  _  _  _  _  _ ",
                            "| || || || || || || || || |",
                            "|_||_||_||_||_||_||_||_||_|"];
 
       assert.equal(0, ocrRecursiveSlice(papersNumbers).checksum);
     });
-    it('345882865 is 0', function () {
+    it('345882865 is 0', () => {
       var papersNumbers = [" _     _  _  _  _  _  _  _ ",
                            " _||_||_ |_||_| _||_||_ |_ ",
                            " _|  | _||_||_||_ |_||_| _|"];
 
       assert.equal(0, ocrRecursiveSlice(papersNumbers).checksum);
     });
-    it('987654321 is 10', function () {
+    it('987654321 is 10', () => {
       var papersNumbers = [" _  _  _  _  _     _  _    ",
                            "|_||_|  ||_ |_ |_| _| _|  |",
                            " _||_|  ||_| _|  | _||_   |"];
@@ -102,8 +102,8 @@ describe('BanckOCR - RecursiveSlice', function() {
       assert.equal(10, ocrRecursiveSlice(papersNumbers).checksum);
     });
   });
-  describe('dirty segment', function () {
-    it('recognize dirty segment', function () {
+  describe('dirty segment', () => {
+    it('recognize dirty segment', () => {
       var papersNumbers = [" _     _  _  _  _  _  _  _ ",
                            " _||_||  |_||   _||_||_ |_ ",
                            " _|  | _||_||_||_  _ |_| _|"];
@@ -111,8 +111,8 @@ describe('BanckOCR - RecursiveSlice', function() {
       assert.equal("34?8?2?65 ILL", ocrRecursiveSlice(papersNumbers).toString());
     });
   });
-  describe('wrong account number', function () {
-    it('recognize wrong checksum', function () {
+  describe('wrong account number', () => {
+    it('recognize wrong checksum', () => {
       var papersNumbers = [" _  _  _  _  _     _  _    ",
                            "|_||_|  ||_ |_ |_| _| _|  |",
                            " _||_|  ||_| _|  | _||_   |"];
